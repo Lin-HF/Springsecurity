0a65bc79aaf80ee6b0d1a6c1903fc38d9defe993
David Lin <haiflin@iu.edu>
2019-07-03 18:41:35 -0400

    get and post

---
+++ imooc-security-demo/src/main/java/com/imooc/dto/UserQueryCondition.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.imooc.dto;
+
+/**
+ * @author linhaifeng1
+ *
+ */
+public class UserQueryCondition {
+	private String username;
+	
+	private int age;
+	
+	private int ageTo;
+	
+	private String xxx;
+
+	public String getXxx() {
+		return xxx;
+	}
+
+	public void setXxx(String xxx) {
+		this.xxx = xxx;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getAgeTo() {
+		return ageTo;
+	}
+
+	public void setAgeTo(int ageTo) {
+		this.ageTo = ageTo;
+	}
+	
+}
